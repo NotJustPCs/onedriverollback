d724df87c7f8d84ba9f835229cba6abe95e79782
Peter Fossey <peter@notjustpcs.co.uk>
2022-05-04 08:25:26 +0100

    Second forum suggestion
    "The list view threshold error usually happens if you have more than 5000 items/files in the document library."

--- onedriverollback.ps1
+++ onedriverollback.ps1
@@ -1,34 +1,45 @@
-#Import SharePoint Online module
- Import-Module Microsoft.Online.SharePoint.Powershell
-     
- Function Restore-PreviousVersion()
- {
-   param
-     (
-         [Parameter(Mandatory=$true)] [string] $SiteURL,
-         [Parameter(Mandatory=$true)] [string] $ListName
-     )
-    Try {
-         $Cred= Get-Credential
-         $Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Cred.Username, $Cred.Password)
-     
-         #Setup the context
-         $Ctx = New-Object Microsoft.SharePoint.Client.ClientContext($SiteURL)
-         $Ctx.Credentials = $Credentials
-             
-         #Get all items from the list/library
-         $Query = [Microsoft.SharePoint.Client.CamlQuery]::CreateAllItemsQuery()
-         $List = $Ctx.Web.Lists.GetByTitle($ListName)
-         $Ctx.Load($List)
-         $ctx.ExecuteQuery()
-         Write-Host "Total number of Items Found in the list:" $List.ItemCount
+  #Import SharePoint Online module
+  Import-Module Microsoft.Online.SharePoint.Powershell
+         
+  Function Restore-PreviousVersion()
+  {
+    param
+      (
+          [Parameter(Mandatory=$true)] [string] $SiteURL,
+          [Parameter(Mandatory=$true)] [string] $ListName
+      )
+     Try 
+     {
+          $Cred= Get-Credential
+          $Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Cred.Username, $Cred.Password)
+         
+          #Setup the context
+          $Ctx = New-Object Microsoft.SharePoint.Client.ClientContext($SiteURL)
+          $Ctx.Credentials = $Credentials
+                 
+          $List = $Ctx.Web.Lists.GetByTitle($ListName)
+          $Ctx.Load($List)
+          $ctx.ExecuteQuery()
+    
+              #Define Query to get List Items in batch
+          $Query = New-Object Microsoft.SharePoint.Client.CamlQuery
+          $Query.ViewXml = @"
+          <View Scope='RecursiveAll'>
+             <Query>
+                 <OrderBy><FieldRef Name='ID' Ascending='TRUE'/></OrderBy>
+             </Query>
+             <RowLimit Paged="TRUE">2000</RowLimit>
+         </View>
+ "@
+             #Get List Items in Batch
+     Do
+     {
          $ListItems = $List.GetItems($Query)
          $Ctx.Load($ListItems)
-         Write-Host "Loaded ListItems into Context"
          $Ctx.ExecuteQuery()
-         Write-Host "Excecuted Context Query"
-     
-         #Iterate through each item and restore the previous version
+         $ListItems.count
+         $Query.ListItemCollectionPosition = $ListItems.ListItemCollectionPosition
+                  #Iterate through each item and restore the previous version
          Foreach($Item in $ListItems)
          { 
              #Get the file versions
@@ -36,12 +47,12 @@
              $Ctx.Load($File)
              $Ctx.Load($File.Versions)
              $Ctx.ExecuteQuery()
-     
+         
              If($File.Versions.Count -gt 0)
              {
                  #Get the previous version's label
                  $VersionLabel=$File.Versions[($File.Versions.Count-1)].VersionLabel
-     
+         
                  #Restore the previous version
                  $File.Versions.RestoreByLabel($VersionLabel)
                  $Ctx.ExecuteQuery()
@@ -52,15 +63,17 @@
                  Write-host "No Versions Available for "$Item["FileRef"] -f Yellow
              }
          }
-      }
-     Catch {
-         write-host -f Red "Error Removing User from Group!" $_.Exception.Message
      }
- } 
-     
- #Set parameter values
- $SiteURL="<Site_URL>"
- $ListName="Documents"
-     
- #Call the function to restore previous document version
- Restore-PreviousVersion -SiteURL $SiteURL -ListName $ListName
+     While($Query.ListItemCollectionPosition -ne $null)
+     }
+      Catch {
+          write-host -f Red "Error Getting List Items:" $_.Exception.Message
+      }
+  } 
+         
+  #Set parameter values
+  $SiteURL="<Site_URL>"
+  $ListName="Documents"
+         
+  #Call the function to restore previous document version
+  Restore-PreviousVersion -SiteURL $SiteURL -ListName $ListName
